709665f954f96c593bd58c5deeba0fb39f945369
Gamal Mohamed <gmohamed@stevens.edu>
2020-12-02 17:20:08 -0500

    Update main.py
    Adjusted main code execution and added graceful process ending on keyboard interrupt

--- src/main.py
+++ src/main.py
@@ -5,24 +5,24 @@
 # GPIOZero - Control of GPIO pins for sensors, lamp, pump, etc. and reading MCP3008 channels
 # CircuitPythonDHT - Reading temperature/humidity data from Adafruit DHT11 Sensor
 # Board - Pin ids for DHT sensor
-# Multiprocessing - for running multple processes (sensing, web server, image capture)
-# Pygame.camera - image capture through OpenCV
+# Multiprocessing, threading - for running multitasking (sensing, web server, image capture)
+# Flask - Web server and network access
+# OpenCV - image capture from USB web cam
 
+# Monitor functions libraries
 from sensor import *
 from database import *
 from control import *
-#from camera import *
-
-
+# Flask web server libraries
 from flask_server import *
 from flask import Flask, render_template, Response
-#import camera_opencv
-#from camera_opencv import VideoCamera
-
-
+# Misc. librariries
 from time import sleep
 import board
 import multiprocessing
+import threading
+import sys
+import os
 
 ##################
 # INITIALIZATION #
@@ -42,79 +42,65 @@ app = Flask(__name__)
 # PROCESSES #
 #############
 def sensors():
-    #while True:
-        #print("Light Sensor")
-        #print(light_sensor.read())
-        #print("Moisture Sensor")
-        #print(moisture_sensor.read())
-        #sleep(1)
-    pass
-        
+    while True:
+        print("Light Sensor")
+        print(light_sensor.read())
+        print("Moisture Sensor")
+        print(moisture_sensor.read())
+        sleep(5)
+       
+server = FlaskServer()
 def flask_server():
-    server = FlaskServer()
-    server.app.run(host='0.0.0.0',port='5000', debug=True)
+    server.app.run(host='0.0.0.0',port='5000', debug=True, use_reloader=False)
 
 def test():
     while True:
         print("Hello :)")
         sleep(0.5)
-    
-process_sensors = multiprocessing.Process(target=sensors) 
-process_sensors.start()
 
-process_flask = multiprocessing.Process(target=flask_server) 
-process_flask.start()
+def main():
+    # Sensor data gathering process
+    process_sensors.start()
+
+def end_processes():
+    process_sensors.terminate()
+    process_sensors.join()
+
+    process_flask.terminate()
+    process_flask.join()
 
 #####################
 # MAIN PROGRAM LOOP #
 #####################
-main_loop_flag = True
-count = 0
-while(main_loop_flag):
-    ##########
-    # CAMERA #
-    ##########
-    #photo = cameraControl.takePhoto()
-    #cameraControl.addToTimeLapse(photo)
-    #cameraControl.updateLiveFeed(photo)
-    ##########
-    # SENSOR #
-    ##########
-    #print("Light Sensor")
-    #print(lightSensor.read())
-    #print("Moisture Sensor")
-    #print(moistureSensor.read())
-    ############
-    # DATABASE #
-    ############
+if __name__ == '__main__':
+    print(__name__)
+    lamp_relay.turn_on()
     sleep(1)
-    if (count < 15):
-        count += 1
-    else:
-        main_loop_flag = False
-    
-################
-# TEST SECTION #
-################
+    lamp_relay.turn_off()
+    try:
+        # start sensor processes
+        process_sensors = multiprocessing.Process(target=sensors)
+        process_sensors.start()
+        print("sensors started")        
+        # start flask process
+        process_flask = multiprocessing.Process(target=flask_server)
+        process_flask.start()
+        # loop until keyboard interrupt is thrown
+        main_loop_flag = True
+        while main_loop_flag:
+            print("Hello :)")
+            sleep(0.5)
+            pass
+    except KeyboardInterrupt: 
+        # terminate sensor process
+        process_sensors.terminate()
+        process_sensors.join()
+        print("sensors ended")
+        # terminate flask process
+        process_flask.terminate()
+        process_flask.join()
+        print("flask ended")
+        # exit program
+        print("Exiting program...")
+        sys.exit(0)
 
-
-#print("Temperature")
-#print(tempSensor.dhtDevice.temperature)
-#test = tempSensor.dhtDevice.temperature
-lamp_relay.turn_on()
-sleep(1)
-lamp_relay.turn_off()
-
-#camera_control.start_cam()
-#image = camera_control.take_photo()
-#camera_control.save_photo(image, 'img/picture.jpg')
-#camera_control.stop_cam()
-
-#################
-# END PROCESSES #
-#################
-process_sensors.join()
-process_sensors.terminate()
-
-process_flask.join()
-process_flask.terminate()
